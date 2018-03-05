9e35e95c3e80eb5e8a56551fcb75346ad4de6eef
Dave Barter <dave@phased.co.uk>
2018-03-05 17:54:47 +0000

    Fix bug with tmp directory and include a more extensive config.json

--- create_tiles/make_vector_tiles.pl
+++ create_tiles/make_vector_tiles.pl
@@ -5,27 +5,40 @@ use Getopt::Long;
 use File::Slurp;
 use Data::Dumper;
 use File::Basename;
-
+use File::Path qw(make_path);
 my $config = "./config.json";
-GetOptions('database=s' => \$config);
+my $skip_geo ='n';
+GetOptions('config=s' => \$config, 'skip_geo=s' => \$skip_geo);
 
 
 my $json = JSON->new->utf8;
+
+
+print "READING $config\n";
 
 my $json_text   = read_file($config);
 my $layers = decode_json( $json_text );
 my @tippecanoe;
 
+# We need a tmp directory to mess about in
+if(! -e $layers->{'config'}->{'tmp_dir'} ){
+    make_path($layers->{'config'}->{'tmp_dir'});
+} else {
+    print "CLEANING TMP DIR\n";
+    system('rm -rf '. $layers->{'config'}->{'tmp_dir'}.'*');
+}
+
 #First make our temp GeoJSON files
 
 foreach my $keys (keys %{$layers->{'layers'}}) {
 
-        #$keys contains hashname and points to an array of layers
-        foreach my $layer (@{$layers->{'layers'}->{$keys}}) {
-                print "Constructing from source " . $layer->{'name'} . "\n";
-                 push (@tippecanoe, make_geojson($layer, $layers->{'config'}->{'tmp_dir'}, $keys));
+    print "KEY $keys\n";
+    #$keys contains hashname and points to an array of layers
+    foreach my $layer (@{$layers->{'layers'}->{$keys}}) {
+        print "Constructing from source " . $layer->{'name'} . "\n";
+        push (@tippecanoe, make_geojson($layer, $layers->{'config'}->{'tmp_dir'}, $keys, $skip_geo));
 
-        }
+    }
 }
 
 #Now let's get canoeing
@@ -34,50 +47,64 @@ my $tippecanoe_command = 'tippecanoe -Z ' . $layers->{'config'}->{'minzoom'} . '
 
 foreach my $geojson (@tippecanoe) {
 
-        $tippecanoe_command .= ' ' . $geojson;
+    $tippecanoe_command .= ' ' . $geojson;
 }
 
 $tippecanoe_command .= ' -f -o ' . $layers->{'config'}->{'output_file'};
 
-#print "$tippecanoe_command\n";
+print "$tippecanoe_command\n";
+my $tipp = `$tippecanoe_command`;
 
 sub make_geojson {
-        my $layer = shift;
-        my $tmp_dir = shift;
+    my $layer = shift;
+    my $tmp_dir = shift;
     my $tippecanoe_layer = shift;
+    my $skip_geo = shift;
 
-        my $command = 'ogr2ogr -f GeoJSON';
+    my $command = 'ogr2ogr -f GeoJSON';
 
-        #source SRS
-        $command .= ' -s_srs ' . (exists($layer->{'s_srs'}) ? $layer->{'s_srs'} : 'EPSG:27700');
-        $command .= ' -t_srs ' . (exists($layer->{'t_srs'}) ? $layer->{'t_srs'} : 'EPSG:4326');
+    #source SRS
+    $command .= ' -s_srs ' . (exists($layer->{'s_srs'}) ? $layer->{'s_srs'} : 'EPSG:27700');
+    $command .= ' -t_srs ' . (exists($layer->{'t_srs'}) ? $layer->{'t_srs'} : 'EPSG:4326');
 
-        #output file name
-        my($filename, $dirs, $suffix) = fileparse($layer->{'file'}, '.shp');
-        $filename = $tmp_dir . $filename . '.geojson';
+    #output file name
+    my($filename, $dirs, $suffix) = fileparse($layer->{'file'}, '.shp');
 
-        #Get layer name for attribite selection
-        my $get_layer_name = 'ogrinfo -al -so ' . $layer->{'file'} . ' | grep \'Layer name\'';
-        my $layer_name = `$get_layer_name`;
-        $layer_name =~ s/Layer name:|\R| //g;
+    print "FILENAME $filename\n";
 
-        print "LAYER $layer_name\n";
+    $filename = $tmp_dir . $filename . '.geojson';
 
-        #attributes we want
-        $command .=' -dialect SQLite -sql ' . (exists($layer->{'attrs'}->[0]) ? '"SELECT '. join(',', @{$layer->{'attrs'}}) . ',geometry FROM ' . $layer_name.'" ' : '"SELECT geometry FROM ' . $layer_name.'" ');
+    return '-L ' . $tippecanoe_layer . ':' . $filename if($skip_geo eq 'y');
 
-        #final command construction
-        $command .= $filename . '.tmp ' . $layer->{'file'};
-  #Remove existing tmp file
-         unlink($filename . '.tmp');
-         my $result = `$command`;
-         print "$layer_name". ($result eq '' ? " OGR complete\n" : " OGR failed $result\n");
+    #Get layer name for attribite selection
+    my $get_layer_name = 'ogrinfo -al -so ' . $layer->{'file'} . ' | grep \'Layer name\'';
+    my $layer_name = `$get_layer_name`;
+    $layer_name =~ s/Layer name:|\R| //g;
 
-         #Now add in zoom details
-         my $jqcommand = 'jq \'.features[].tippecanoe.minzoom = ' . $layer->{'minzoom'} . '| .features[].tippecanoe.maxzoom = ' . $layer->{'maxzoom'} .'\' ' . $filename . '.tmp > ' . $filename;
+    print "LAYER $layer_name\n";
 
-         $result = `$jqcommand`;
-         print "$layer_name". ($result eq '' ? " JQ complete\n" : " JQ failed $result\n");
-         return '-L ' . $tippecanoe_layer . ':' . $filename;
+    #attributes we want
+    $command .=' -dialect SQLite -sql ' . (exists($layer->{'attrs'}->[0]) ? '"SELECT '. join(',', @{$layer->{'attrs'}}) . ',geometry FROM ' . $layer_name.'" ' : '"SELECT geometry FROM ' . $layer_name.'" ');
 
- }
+    #final command construction
+    $command .= $filename . '.tmp ' . $layer->{'file'};
+
+    #Remove existing tmp file
+    #unlink($filename . '.tmp');
+    print "OGR COMMAND $command\n";
+    my $result = `$command`;
+    print "$layer_name". ($result eq '' ? " OGR complete\n" : " OGR failed $result\n");
+
+    #Now add in zoom details
+
+    #my $jqcommand = 'jq \'.features[].tippecanoe.minzoom = ' . $layer->{'minzoom'} . '| .features[].tippecanoe.maxzoom = ' . $layer->{'maxzoom'} .'\' ' . $filename . '.tmp > ' . $filename;
+
+    #TRY USING SED FOR SPEED
+
+    my $jqcommand = 'sed \'s#"properties"#"tippecanoe" :{"minzoom" : '. $layer->{'minzoom'} .',"maxzoom" : '. $layer->{'maxzoom'} . '},"properties"#g\' '. $filename . '.tmp > ' . $filename;
+    print "JQ $jqcommand\n";
+    $result = `$jqcommand`;
+    print "$layer_name". ($result eq '' ? " JQ complete\n" : " JQ failed $result\n");
+    return '-L ' . $tippecanoe_layer . ':' . $filename;
+
+}
